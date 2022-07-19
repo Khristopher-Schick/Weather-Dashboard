ede0921fc8e70338c4b540cf29453f9173dbade3
Khristopher-Schick <104205888+Khristopher-Schick@users.noreply.github.com>
2022-07-19 19:15:12 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+ function weather () {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=2cc8df7d9aaf0caf0291a295fae04fe0")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log();
+    //   document.querySelector(".timeDisplay").innerHTML = `Time: ${time}`;
+
+    });
+  };
+//   weather ()
+
+
+
+
